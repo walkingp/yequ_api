68180c1523c34d37999c39bb4945412816fd5045
Hong Jian Michael Wang <Hong.Jian.Michael.Wang@dnvgl.com>
2020-09-14 10:35:14 +0800

    now setting

--- curl.js
+++ curl.js
@@ -3,8 +3,16 @@ var https = require("https");
 // Utility function that downloads a URL and invokes
 // callback with the data.
 function download(url, callback) {
+  var options = {
+    url,
+    headers: {
+      "Content-Type": "text/html",
+      "Content-Encoding": "identity",
+      Charset: "utf-8",
+    },
+  };
   https
-    .get(url, function (res) {
+    .get(options, function (res) {
       var data = "";
       res.on("data", function (chunk) {
         data += chunk;
