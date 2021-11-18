13b386227dca937326f36833d9f2953f9ec25405
Hong.Jian.Michael.Wang <Hong.Jian.Michael.Wang@dnv.com>
2021-11-18 10:46:25 +0800

    fix: change domain

--- config/config.js
+++ config/config.js
@@ -2,13 +2,13 @@ module.exports = {
   tulip: {
     client_id: "159894829619272",
     client_secret: "R6wl7XZcQbNn2bsNt5FlChi61cLvXchA",
-    redirect_uri: "https://api.luwan.club/api/v1/tulip/callback",
+    redirect_uri: "https://api.xterra.club/api/v1/tulip/callback",
     apiUrl: "https://open.tulipsport.com",
   },
   strava: {
     client_id: "22580",
     client_secret: "427267881c103f35f75d833992a92fc50226c14d",
-    redirect_uri: "https://api.luwan.club/api/v1/strava/callback",
+    redirect_uri: "https://api.xterra.club/api/v1/strava/callback",
     apiUrl: "https://www.strava.com/api/v3",
   },
   wechat: {
--- routes/spider.js
+++ routes/spider.js
@@ -1,13 +1,50 @@
 const express = require("express");
+var request = require("request");
+var fs = require("fs");
+var path = require("path");
 const router = express.Router();
 const rp = require("request-promise");
 const puppeteer = require("puppeteer");
+const cheerio = require("cheerio");
+
+function matchPics(html) {
+  const result = html.match(/data-src="(.*?)"/gi);
+  var arr = [];
+  result.forEach((item) => {
+    if (item.indexOf("https://") >= 0) {
+      arr.push(item.replace("data-src=", "").replace('"', ""));
+    }
+  });
+  return arr;
+}
+var downloadPic = function (src, dest) {
+  var dirPath = path.join(__dirname, "file");
+  request(src)
+    .pipe(fs.createWriteStream(path.join(dirPath, dest)))
+    .on("close", function () {
+      console.log("pic saved!");
+    });
+};
 
 router.get("/", async function (req, res) {
   const { url } = req.query;
   try {
     const html = await rp(url);
-    res.send(html);
+    const $ = cheerio.load(html, { ignoreWhitespace: true });
+
+    const allList = matchPics(html);
+    allList.forEach((url, index) => {
+      console.log("saving:", url);
+      const fix = url.slice(url.lastIndexOf("=") + 1);
+      downloadPic(url, new Date().getTime() + "." + fix);
+    });
+    let content = $("#js_content").html().trim();
+    content = unescape(content.replace(/&#x/g, "%u").replace(/;/g, ""));
+    res.send({
+      title: $("#activity-name").text().trim(),
+      publish_time: $("#publish_time").text(),
+      content,
+    });
   } catch (err) {
     res.status(401).send({
       msg: err,
