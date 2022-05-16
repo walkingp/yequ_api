a2391111fdb4df65b8ca0f59cc580e300cceaaad
walkingp <walkingp@126.com>
2022-05-16 10:13:47 +0800

    config

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(express.static(__dirname));
 
 //this is the base route
 app.get("/", async function (req, res) {
-  res.send(`hello`);
+  res.send(`View more on https://luwan.vercel.app/`);
 });
 app.get("/tulip_bindok.html", async function (req, res) {
   const data = fs.readFileSync(__dirname + "/assets/tulip_bindok.html");
@@ -28,6 +28,9 @@ app.get("/tulip_bindok.html", async function (req, res) {
 });
 app.get("/.well-known/pki-validation/fileauth.txt", async function (req, res) {
   res.send("202010011352470e6j0043sgasfqeb8s8z2qifzm5pxauvb95p2371jgm7zr7tje");
+});
+app.get("mHWu41fGmo.txt", async function (req, res) {
+  res.send("27a58a497641148b3232d9006647f5ca");
 });
 
 const tulip = require("./routes/tulip");
